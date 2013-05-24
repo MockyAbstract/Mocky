1e014cedba9e1ed654b13faa7ca5306955aeb2f1
Nicolas Deverge <ndeverge@ekito.fr>
2013-05-24 16:21:27 +0200

    Introducing Cake Pattern

--- app/services/IRepository.scala
+++ app/services/IRepository.scala
@@ -6,23 +6,9 @@ import scala.concurrent.Future
 trait IRepository {
 
   def getMockFromId(id: String): Future[MockResponse]
-  def save(mock: Mocker): Future[String]
+  def saveMock(mock: Mocker): Future[String]
 
   def encodeBody(content: String, charset: String = "UTF-8"): String
   def decodeBody(content: String, charset: String = "UTF-8"): String
 }
 
-object Repository {
-
-  lazy val version = play.api.Play.current.configuration.getString("version").getOrElse("undefined")
-
-  def apply(version: String) = {
-    version match {
-      case "beta" | "v1" => GithubRepository
-      case "fs" => FileSystemRepository
-      case _ => MongoRepository
-    }
-  }
-
-  def current = apply(version)
-}
