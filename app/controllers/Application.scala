56ca464f3d87acc65003623a5422bdb63f0ba351
Julien Lafont <yotsumi.fx@gmail.com>
2017-10-21 22:20:02 +0200

    Support delay parameter

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,43 +1,71 @@
 package controllers
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
 import javax.inject.Inject
 
+import akka.pattern.after
 import play.api.http.ContentTypes
 import play.api.i18n.{I18nSupport, Lang}
 import play.api.libs.json._
 import play.api.mvc._
+import akka.actor.ActorSystem
 
 import models._
-import services.RepositoryDispatcher
+import services.{IRepository, RepositoryDispatcher}
 
-class Application @Inject()(cc: ControllerComponents, dispatcher: RepositoryDispatcher)(implicit ec: ExecutionContext)
-  extends AbstractController(cc) with I18nSupport {
+class Application @Inject()(cc: ControllerComponents, dispatcher: RepositoryDispatcher)
+    (implicit ec: ExecutionContext, actorSystem: ActorSystem)
+    extends AbstractController(cc) with I18nSupport {
 
   private val defaultError = Future(InternalServerError)
 
-  def index = Action {
+  def index = Action { implicit req =>
     Ok(views.html.index(Mocker.formMocker))
   }
 
-  def get(id: String, version: String) = Action.async { request =>
+  def get(id: String, version: String) = Action.async { implicit request =>
     val repo = dispatcher(version)
+    val responseFromMock = prepareMockResponse(repo, request) _
 
-    val jsonpCallback = request.queryString.get("callback").flatMap(_.headOption)
+    val response = repo.getMockFromId(id).map(responseFromMock).fallbackTo(defaultError)
+    addDelay(response)
+  }
 
-    repo.getMockFromId(id).map { mock =>
-      val body = repo.decodeBody(mock.content, mock.metadata.charset)
+  private def prepareMockResponse(repo: IRepository, req: RequestHeader)(mock: MockResponse): Result = {
+    val body = repo.decodeBody(mock.content, mock.metadata.charset)
+    val jsonpCallback = extractJsonpParameter(req)
 
-      // add jsonp wrapper is required
-      val (bodyWithJsonP, contentType) = jsonpCallback match {
-        case Some(callback) => (s"$callback($body);", ContentTypes.JAVASCRIPT)
-        case None => (body, mock.metadata.contentType)
-      }
+    // add jsonp wrapper is required
+    val (bodyWithJsonP, contentType) = jsonpCallback match {
+      case Some(callback) => (s"$callback($body);", ContentTypes.JAVASCRIPT)
+      case None => (body, mock.metadata.contentType)
+    }
 
-      Status(mock.metadata.status)(bodyWithJsonP)
-        .withHeaders(mock.metadata.headersWithoutContentType: _*)
-        .as(contentType)
-    }.fallbackTo(defaultError)
+    Status(mock.metadata.status)(bodyWithJsonP)
+      .withHeaders(mock.metadata.headersWithoutContentType: _*)
+      .as(contentType)
+  }
+
+  private def addDelay(response: Future[Result])(implicit req: RequestHeader): Future[Result] = {
+    extractDelayParameter(req) match {
+      case Some(duration) => after(duration, actorSystem.scheduler)(response)
+      case _ => response
+    }
+  }
+
+  private def extractJsonpParameter(req: RequestHeader): Option[String] = {
+    req.queryString.get("callback").flatMap(_.headOption)
+  }
+
+  private def extractDelayParameter(req: RequestHeader): Option[FiniteDuration] = {
+    req.queryString.get("mocky-delay").flatMap(_.headOption.map(Duration.apply)).collect {
+      case d: FiniteDuration => d
+    }.filter {
+      case d if d.lt(Duration.Zero) => false
+      case d if d.gt(60.seconds) => false
+      case _ => true
+    }
   }
 
   def save = Action.async { implicit request =>
