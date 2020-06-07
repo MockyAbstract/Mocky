c854ac5e8bf39c68af7d6085d428357fd6df5d68
Julien Lafont <yotsumi.fx@gmail.com>
2020-06-07 19:06:45 +0200

    Logs unauthorized access to admin route

--- server/src/main/scala/io/mocky/http/middleware/Authorization.scala
+++ server/src/main/scala/io/mocky/http/middleware/Authorization.scala
@@ -2,6 +2,8 @@ package io.mocky.http.middleware
 
 import cats.data.{ Kleisli, OptionT }
 import cats.effect.IO
+import io.chrisdavenport.log4cats.SelfAwareStructuredLogger
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import org.http4s.implicits._
 import org.http4s.server.AuthMiddleware
 import org.http4s.{ AuthedRoutes, Request, Response, Status }
@@ -15,14 +17,16 @@ sealed trait AuthenticationError {
   val message: String
   val status: Status
 }
-case class Unauthorized(message: String) extends AuthenticationError {
+final case class Unauthorized(message: String) extends AuthenticationError {
   val status = Status.Unauthorized
 }
-case class Forbidden(message: String) extends AuthenticationError {
+final case class Forbidden(message: String) extends AuthenticationError {
   val status = Status.Forbidden
 }
 
 class Authorization(settings: AdminSettings) {
+
+  private val logger: SelfAwareStructuredLogger[IO] = Slf4jLogger.getLogger[IO]
 
   /**
     * Basic Admin authorization, working by sending a token in `settings.admin.header` header that
@@ -31,7 +35,14 @@ class Authorization(settings: AdminSettings) {
   val Administrator = AuthMiddleware(authAdministrator, onFailure)
 
   private def onFailure: AuthedRoutes[AuthenticationError, IO] = {
-    Kleisli(req => OptionT.liftF(IO.pure(Response[IO](req.context.status).withEntity(req.context.message))))
+    Kleisli { req =>
+      OptionT.liftF {
+        for {
+          _ <- logger.warn(s"Unauthorized access to ADMIN route: ${req.context.message} from ${req.req}")
+          response <- IO.pure(Response[IO](req.context.status).withEntity(req.context.message))
+        } yield response
+      }
+    }
   }
 
   private def authAdministrator: Kleisli[IO, Request[IO], Either[AuthenticationError, Role]] = Kleisli {
@@ -39,7 +50,8 @@ class Authorization(settings: AdminSettings) {
       IO.pure(for {
         header <- request.headers.get(settings.header.ci).toRight(
           Unauthorized("Couldn't find the authorization header"))
-        res <- Either.cond(header.value == settings.password, Admin, Forbidden("Invalid token"))
+        res <- Either.cond(header.value == settings.password, Admin, Forbidden(s"Invalid token '${header.value}'"))
       } yield res)
   }
+
 }
