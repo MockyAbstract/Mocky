3929b89a376ba93c870d492e3aa8080e466cf44f
Julien Lafont <julien.lafont@tabmo.io>
2020-11-28 14:14:04 +0100

    Remove console.log

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,7 +10,6 @@ import * as serviceWorker from './serviceWorker';
 import GA from './services/Analytics/GA';
 
 GA.initialize();
-console.log(process.env);
 
 ReactDOM.render(
   <Provider store={store}>
